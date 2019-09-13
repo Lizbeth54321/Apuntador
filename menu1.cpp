9cee1061d3fe0e45205c0845bc4765d5d12f72ff
Lizbeth54321 <lm4932830@gmail.com>
2019-09-12 22:00:15 -0500

    La modificacion que se realizo fue  para implemetar un menu en la cual se implemeta los apuntadores y los areglos destro de una estructura en la que se pide el nombre , subscipcion y el tiempo que de subcripcion de la persona

---
+++ menu1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<locale>
+using namespace std;
+
+struct persona{
+    char nombre[20];
+    int suscripcion;
+    char revista[20];
+};
+
+int main(){
+    char *locale;
+    locale=setlocale(LC_ALL,"");
+    struct persona P[10];
+    struct persona *apuntador;
+    apuntador=P;
+    int opcion,c;
+    char eliminar[20];
+    for(int i=0; i<10; i++)
+        P[i].nombre[0]='n';
+    cout<<"\n\t\tMENÚ\n 1. Dar de alta un suscriptor\n 2. Eliminar suscripcion \n 3. Salir\n¿Opcion? "; cin>>opcion;
+    switch (opcion){
+        case 1:
+            for(int i=0;i<10; i++){
+                if(P[i].nombre[0]=='n'){
+                    cout<<"Nombre: "; fflush(stdin);cin.getline(P[i].nombre,20,'\n');
+                    cout<<"Suscripcion(meses): "; cin>>P[i].suscripcion;
+                    cout<<"Nombre de la revista: "; fflush(stdin); cin.getline(P[i].revista,20,'\n');
+                    cout<<'\n';
+                    break;
+                    }else
+                    cout<<"El "<<i+1<<"º espacio esta ocupado"<<endl;
+        }break;
+
+        case 2:
+            cout<<"Gracias!"<<endl;
+            break;
+
+    }
+
+    }
+                    
+                    
